db562b5fd943a5d664516d6a943ea576edf9bc95
wjwei-handsome <wjwei@webmail.hzau.edu.cn>
2020-06-05 20:39:54 +0800

    6.5日

--- example2_housing.py
+++ example2_housing.py
@@ -133,3 +133,28 @@ full_pipeline = ColumnTransformer([
 housing_prepared = full_pipeline.fit_transform(housing)
 housing_prepared.shape
 
+#选择线性回归模型进行训练
+from sklearn.linear_model import LinearRegression
+lin_reg=LinearRegression()
+lin_reg.fit(housing_prepared,housing_labels)
+some_data=housing.iloc[:5] #挑选训练集的数据试试看
+some_labels=housing_labels.iloc[:5]
+some_data_prepared=full_pipeline.transform(some_data)
+lin_reg.predict(some_data_prepared)
+some_labels #两者相比差的有点多
+
+#测量RMSE 看看训练效果
+from sklearn.metrics import mean_squared_error
+housing_predictions=lin_reg.predict(housing_prepared)
+lin_mse=mean_squared_error(housing_labels,housing_predictions) #计算实际和预测的误差
+lin_rmse=np.sqrt(lin_mse)
+lin_rmse
+
+#换一个模型 决策树模型
+from sklearn.tree import DecisionTreeRegressor
+tree_reg = DecisionTreeRegressor(random_state=42)
+tree_reg.fit(housing_prepared, housing_labels)
+housing_predictions = tree_reg.predict(housing_prepared)
+tree_mse = mean_squared_error(housing_labels, housing_predictions)
+tree_rmse = np.sqrt(tree_mse)
+tree_rmse
